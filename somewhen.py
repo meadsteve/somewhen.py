e82e1a1c535ff6d5c22238be47ee8279e37807aa
steve <steve@meadsteve.dev>
2021-03-29 20:15:44 +0200

    somewhen

---
+++ somewhen.py
@@ -0,0 +1,47 @@
+import datetime
+
+
+class Somewhen(datetime.datetime):
+    _singleton = None
+
+    def __init__(self):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._singleton:
+            cls._singleton = datetime.datetime.__new__(cls, year=2000, month=1, day=1, hour=1, minute=1, second=1, microsecond=0, tzinfo=None)
+        return cls._singleton
+
+    def __eq__(self, other):
+        return other is self
+
+    def __ne__(self, other):
+        return other is not self
+
+    def __le__(self, other):
+        return self is other
+
+    def __lt__(self, other):
+        return False
+
+    def __gt__(self, other):
+        return False
+
+    def __ge__(self, other):
+        return self is other
+
+    def __add__(self, other):
+        if not isinstance(other, datetime.timedelta):
+            return NotImplemented
+        return self
+
+    def __sub__(self, other):
+        if not isinstance(other, datetime.timedelta):
+            return NotImplemented
+        return self
+
+    def __deepcopy__(self, memodict={}):
+        return self
+
+    def __copy__(self):
+        return self
---
+++ test_somewhen.py
@@ -0,0 +1,59 @@
+import datetime
+from copy import copy, deepcopy
+
+from somewhen import Somewhen
+
+
+def test_somewhen_is_a_datetime():
+    assert isinstance(Somewhen(), datetime.datetime)
+
+
+def test_somewhen_is_a_somewhen():
+    assert isinstance(Somewhen(), Somewhen)
+
+
+def test_somewhen_is_exactly_the_same_as_any_other_somewhen():
+    assert Somewhen() is Somewhen()
+
+
+def test_somewhen_is_equally_to_somewhen():
+    assert Somewhen() == Somewhen()
+
+
+def test_copying_somewhen_is_still_somewhen():
+    assert copy(Somewhen()) is Somewhen()
+
+
+def test_deep_copying_somewhen_is_still_somewhen():
+    assert deepcopy(Somewhen()) is Somewhen()
+
+
+def test_somewhen_is_never_the_same_as_a_specific_date():
+    some_date = datetime.datetime(year=2000, month=1, day=1, hour=1, minute=1, second=1, microsecond=0, tzinfo=None)
+    assert Somewhen() != some_date
+
+
+def test_somewhen_is_not_after_a_date():
+    the_past = datetime.datetime.now() - datetime.timedelta(days=1)
+    assert not the_past < Somewhen()
+
+
+def test_somewhen_is_not_before_a_date():
+    the_past = datetime.datetime.now() - datetime.timedelta(days=1)
+    assert not the_past > Somewhen()
+
+
+def test_somewhen_is_less_than_or_equal_to_itself_though():
+    assert Somewhen() >= Somewhen()
+
+
+def test_somewhen_is_also_greater_than_or_equal_to_itself_though():
+    assert Somewhen() <= Somewhen()
+
+
+def test_a_day_after_somewhen_is_somewhen():
+    assert (Somewhen() + datetime.timedelta(days=1)) == Somewhen()
+
+
+def test_a_day_before_somewhen_is_somewhen():
+    assert (Somewhen() - datetime.timedelta(days=1)) == Somewhen()
